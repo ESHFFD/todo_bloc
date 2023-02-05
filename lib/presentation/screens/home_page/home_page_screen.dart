0e5e540c01a16cf53967416f5c755daf478ac625
ESHFFD <ehsanshamsfallah@gmail.com>
2023-02-05 14:36:32 +0330

    task list is completed and also created two local task

--- lib/presentation/screens/home_page/home_page_screen.dart
+++ lib/presentation/screens/home_page/home_page_screen.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app_bloc/presentation/models/task.dart';
 
-class HomePageScreen extends StatefulWidget {
+class HomePageScreen extends StatelessWidget {
   HomePageScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomePageScreen> createState() => _HomePageScreenState();
-}
-
-class _HomePageScreenState extends State<HomePageScreen> {
+  final List<Task> taskList = [
+    Task(title: 'This is a 1 Task'),
+    Task(title: 'This is a 1 Task'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("todo app"),
       ),
+      body: Column(children: [
+        const Center(
+          child: Chip(label: Text('Tasks:')),
+        ),
+        ListView.builder(
+            itemCount: taskList.length,
+            itemBuilder: (contex, index) {
+              var taskListItems = taskList[index];
+              return ListTile(
+                title: Text(taskListItems.title),
+                trailing: Checkbox(
+                  onChanged: (value) {},
+                  value: null,
+                ),
+              );
+            })
+      ]),
     );
   }
 }
