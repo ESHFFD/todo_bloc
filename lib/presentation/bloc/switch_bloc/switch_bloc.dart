edd0c2e0abf7257118ec0048335df395da0216a7
ESHFFD <ehsanshamsfallah@gmail.com>
2023-02-07 11:05:28 +0330

    add hydrated to theme and it work prefectly

--- lib/presentation/bloc/switch_bloc/switch_bloc.dart
+++ lib/presentation/bloc/switch_bloc/switch_bloc.dart
@@ -1,10 +1,10 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:todo_app_bloc/presentation/bloc/bloc_exports.dart';
 
 part 'switch_event.dart';
 part 'switch_state.dart';
 
-class SwitchBloc extends Bloc<SwitchEvent, SwitchState> {
+class SwitchBloc extends HydratedBloc<SwitchEvent, SwitchState> {
   SwitchBloc() : super(const SwitchInitial(switchValue: false)) {
     on<SwitchOnEvent>(
       (event, emit) {
@@ -16,5 +16,15 @@ class SwitchBloc extends Bloc<SwitchEvent, SwitchState> {
         emit(const SwitchState(switchValue: false));
       },
     );
+  }
+
+  @override
+  SwitchState? fromJson(Map<String, dynamic> json) {
+    return SwitchState.fromMap(json);
+  }
+
+  @override
+  Map<String, dynamic>? toJson(SwitchState state) {
+    return state.toMap();
   }
 }
